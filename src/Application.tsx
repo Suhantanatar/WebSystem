e75da2918112ac8b0246331e36752d626ed0617e
BAS1C010 <sb.tanatar@gmail.com>
2026-03-12 17:49:33 +0100

    oppgave 1 gjort

---
+++ src/Application.tsx
@@ -0,0 +1,123 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Feature, Map, MapBrowserEvent, Overlay, View } from "ol";
+import { OSM } from "ol/source.js";
+import TileLayer from "ol/layer/Tile.js";
+import { useGeographic } from "ol/proj.js";
+
+import "ol/ol.css";
+import "./application.css";
+import VectorLayer from "ol/layer/Vector.js";
+import VectorSource from "ol/source/Vector.js";
+import { GeoJSON } from "ol/format.js";
+import { Circle, Fill, Stroke, Style, Text } from "ol/style.js";
+import type { FeatureLike } from "ol/Feature.js";
+import { Point } from "ol/geom.js";
+
+useGeographic();
+
+const fylkeSource = new VectorSource({
+  url: `${import.meta.env.BASE_URL}geojson/fylker.geojson`,
+  format: new GeoJSON(),
+});
+
+const vgsSource = new VectorSource({
+  url: `${import.meta.env.BASE_URL}geojson/vgs.geojson`,
+  format: new GeoJSON(),
+});
+
+const map = new Map({
+  layers: [
+    new TileLayer({ source: new OSM() }),
+    new VectorLayer({
+      source: fylkeSource,
+      style: new Style({
+        stroke: new Stroke({ color: "black", width: 1 }),
+      }),
+    }),
+    new VectorLayer({
+      source: vgsSource,
+      style: new Style({
+        image: new Circle({
+          radius: 6,
+          stroke: new Stroke({ color: "black" }),
+          fill: new Fill({ color: "red" }),
+        }),
+      }),
+    }),
+  ],
+  view: new View({
+    center: [11, 60],
+    zoom: 9,
+  }),
+});
+
+const overlay = new Overlay({
+  positioning: "bottom-center",
+});
+
+export function Application() {
+  const mapRef = useRef<HTMLDivElement | null>(null);
+  const overlayRef = useRef<HTMLDivElement | null>(null);
+
+  const [activeFylke, setActiveFylke] = useState<Feature>();
+  const [selectedSchools, setSelectedSchools] = useState<FeatureLike[]>([]);
+
+  function handlePointerMove(e: MapBrowserEvent) {
+    const features = fylkeSource.getFeaturesAtCoordinate(e.coordinate);
+    setActiveFylke(features.length > 0 ? (features[0] as Feature) : undefined);
+  }
+
+  function handleClick(e: MapBrowserEvent) {
+    const features = map.getFeaturesAtPixel(e.pixel, {
+      layerFilter: (l) => l.getSource() === vgsSource,
+    });
+    setSelectedSchools(features);
+    overlay.setPosition(
+      features.length > 0
+        ? (features[0]!.getGeometry() as Point).getCoordinates()
+        : undefined,
+    );
+  }
+
+  useEffect(() => {
+    activeFylke?.setStyle(activeFylkeStyle);
+    return () => activeFylke?.setStyle(undefined);
+  }, [activeFylke]);
+
+  function activeFylkeStyle(feature: FeatureLike): Style {
+    const fylkesnavn = feature.getProperties()["fylkesnavn"];
+    return new Style({
+      stroke: new Stroke({ color: "black", width: 3 }),
+      text: new Text({
+        text: fylkesnavn,
+        stroke: new Stroke({ color: "white", width: 2 }),
+        font: "bold 24px serif",
+      }),
+    });
+  }
+
+  useEffect(() => {
+    map.setTarget(mapRef.current!);
+    map.on("pointermove", handlePointerMove);
+    overlay.setElement(overlayRef.current!);
+    map.on("click", handleClick);
+    map.addOverlay(overlay);
+
+    return () => {
+      map.un("pointermove", handlePointerMove);
+      map.un("click", handleClick);
+      map.setTarget(undefined);
+    };
+  }, []);
+
+  return (
+    <>
+      <div ref={mapRef} className="map" />
+      <div ref={overlayRef} className="ol-overlay-container">
+        {selectedSchools.length === 1
+          ? selectedSchools[0]!.getProperties()["skolenavn"]
+          : `${selectedSchools.length} valgte skoler`}
+      </div>
+    </>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Application } from "./Application.js";
+
+createRoot(document.getElementById("root")!).render(<Application />);
